3827b3b93ab4c6d847c7dc44eb9d5b1c6fc8445f
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-08 21:06:14 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const path = require ('path');
-const port=8000;
+const port=process.env.PORT || 8000;
 const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
