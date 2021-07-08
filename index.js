79cb89a53930271b5c5fb367c722148da4701fd0
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-08 21:01:01 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const path = require ('path');
+const port=8000;
+const app=express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+
+
+
+
+
+app.get('/',function(req,res){
+    return res.render('abc');
+});
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
