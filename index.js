cae2b036c664e84730ddc1b52b0370969056574b
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 13:21:08 +0530

    before css

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 app.get('/',function(req,res){
     return res.render('abc');
