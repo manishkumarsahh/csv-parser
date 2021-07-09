40b999a797635860dbe50175c360fa44cfd6b7b7
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 15:12:12 +0530

    after introducing mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect(process.env.db || 'mongodb://localhost/codeial_development');
 
 const db = mongoose.connection;
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8000;
+const port =process.env.PORT || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
@@ -23,6 +23,8 @@ app.use('/', require('./routes'));
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
 
 
 app.listen(port, function(err){
