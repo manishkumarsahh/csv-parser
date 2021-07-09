85d5a7764338449d391ac592f75f02349138410d
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 21:12:55 +0530

    mongoose

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const port = process.env.PORT ||  8000;
 
 const app = express();
 
+//changes from here
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority';
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((err)=> console.log(`no connection`));
+
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
