db93982ebaa6c95db9e35fc2bd58b3285ca468ea
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 22:12:21 +0530

    mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,25 @@
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority');
+// const db= mongoose.connection;
+
+
+//changes from here
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority');
-const db= mongoose.connection;
-db.on('error',console.error.bind(console,'error connecting to db'));
-db.once('open',function(){
-    console.log('successfully connected to database');
-});
+const db = 'mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority';
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((err)=> console.log(`no connection`));
+
+
+
+
+
+// db.on('error',console.error.bind(console,'error connecting to db'));
+// db.once('open',function(){
+//     console.log('successfully connected to database');
+// });
