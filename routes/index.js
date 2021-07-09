ac4f3fcfc225522fb53413e5398e9918e7abf530
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 15:55:29 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ console.log('router loaded');
 
 
 router.get('/', homeController.home);
-router.use('/users', require('./users'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
