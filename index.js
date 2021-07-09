5650f77b23c8f92e135df72d07ab4cbe423d8b7f
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-09 17:02:47 +0530

    a

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port =process.env.PORT || 8000;
-const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
@@ -11,10 +10,7 @@ app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
-app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
 
 
 // use express router
