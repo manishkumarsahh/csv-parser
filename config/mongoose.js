bf2170040df16f443abe6bd835b0198cc7ed9c73
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-10 12:47:16 +0530

    mongo changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,10 +7,10 @@
 const mongoose = require('mongoose');
 // const db = process.env.MONGODB_URI || 'mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority';
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://manish:kumar@cluster0.5vaq2.mongodb.net/contactlist?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
+    useNewUrlParser: true
+    // useCreateIndex:true,
+    // useUnifiedTopology:true,
+    // useFindAndModify:false
 }).then(() => {
     console.log(`connection successful`);
 }).catch((err)=> console.log(`no connection`));
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Contact = require('./models/contact');
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('assets'));
 
      
